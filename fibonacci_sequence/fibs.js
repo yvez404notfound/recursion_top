159e2d69b88f0ee6abd064721ef077f6560c61c8
Yves Casio <yveschristianpcasio@gmail.com>
2026-02-12 08:16:29 +0800

    feat: fibonacci sequence solution

--- fibonacci_sequence/fibs.js
+++ fibonacci_sequence/fibs.js
@@ -1,5 +1,23 @@
-const fibs = function () {
-	return "fibs";
+const fibs = function (n) {
+	if (n < 0) return;
+	if (n === 0) return [0];
+	if (n === 1) return [0, 1];
+
+	let sequence = [];
+	for (let i = 0; i < n - 1; i++) {
+		if (sequence.length === 0) sequence.push(0);
+		sequence.push((sequence[i - 1] ?? 1) + sequence[i]);
+	}
+
+	return sequence;
 };
 
-module.exports = fibs;
+const fibsRec = function (n) {
+	if (n > 1) return fibsRec(n - 1) + fibsRec(n - 2);
+	return n;
+};
+
+module.exports = {
+	fibs,
+	fibsRec,
+};
--- fibonacci_sequence/fibs.spec.js
+++ fibonacci_sequence/fibs.spec.js
@@ -1,7 +1,47 @@
-const fibs = require("./fibs");
+const { fibs, fibsRec } = require("./fibs");
 
 describe("Fibonacci Sequence", () => {
-	test("Iteration: 1n", () => {
-		expect(fibs()).toBe("fibs");
+	test("Iteration: 0", () => {
+		expect(fibs(0)).toEqual([0]);
+	});
+
+	test("Iteration: 1", () => {
+		expect(fibs(1)).toEqual([0, 1]);
+	});
+
+	test("Iteration: 8", () => {
+		expect(fibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+	});
+
+	test("Iteration: 16", () => {
+		expect(fibs(16)).toEqual([
+			0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
+		]);
+	});
+
+	test("Negative numbers", () => {
+		expect(fibs(-1)).toBeUndefined();
+	});
+});
+
+describe("Fibonacci Sequence (Recursive)", () => {
+	test("Iteration: 0", () => {
+		expect(fibsRec(0)).toBe(0);
+	});
+
+	test("Iteration: 1", () => {
+		expect(fibsRec(1)).toBe(1);
+	});
+
+	test("Iteration: 8", () => {
+		expect(fibsRec(8)).toBe(21);
+	});
+
+	test("Iteration: 16", () => {
+		expect(fibsRec(16)).toBe(987);
+	});
+
+	test("Negative numbers", () => {
+		expect(fibsRec(-5)).toBe(5);
 	});
 });
